88bb608ba6b1bf2546f368c2fcd641501d0eb7c6
Selena Groh <selena.groh@gmail.com>
2024-09-07 16:16:11 -0400

    update card data to TS file

---
+++ data/card_data.ts
@@ -0,0 +1,180 @@
+export const CARD_DATA = [
+  {
+    name: "Sample White Card Name",
+    cost: "2W",
+    type: {
+      supertype: "Creature",
+      subtype: "",
+    },
+    color: "White",
+    indexNumber: "1",
+  },
+  {
+    name: "Sample Blue Card Name",
+    cost: "2U",
+    type: {
+      supertype: "Creature",
+      subtype: "",
+    },
+    color: "Blue",
+    indexNumber: "2",
+  },
+  {
+    name: "Sample Black Card Name",
+    cost: "2B",
+    type: {
+      supertype: "Creature",
+      subtype: "",
+    },
+    color: "Black",
+    indexNumber: "3",
+  },
+  {
+    name: "Sample Red Card Name",
+    cost: "2R",
+    type: {
+      supertype: "Creature",
+      subtype: "",
+    },
+    color: "Red",
+    indexNumber: "4",
+  },
+  {
+    name: "Sample Green Card Name",
+    cost: "2G",
+    type: {
+      supertype: "Creature",
+      subtype: "",
+    },
+    color: "Green",
+    indexNumber: "5",
+  },
+  {
+    name: "Sample Gold Card Name",
+    cost: "WB",
+    type: {
+      supertype: "Creature",
+      subtype: "",
+    },
+    color: "Gold",
+    indexNumber: "6",
+  },
+  {
+    name: "Sample Artifact Card Name",
+    cost: "2",
+    type: {
+      supertype: "Artifact",
+      subtype: "",
+    },
+    color: "Artifact",
+    indexNumber: "7",
+  },
+  {
+    name: "Sample Land Card Name",
+    cost: "",
+    type: {
+      supertype: "Land",
+      subtype: "",
+    },
+    color: "Land",
+    indexNumber: "8",
+  },
+];
+export const CARD_DATA_BY_COLOR = {
+  White: [
+    {
+      name: "Sample Card Name",
+      cost: "2W",
+      type: {
+        supertype: "Creature",
+        subtype: "",
+      },
+      color: "White",
+      indexNumber: "1",
+    },
+  ],
+  Blue: [
+    {
+      name: "Sample Blue Card Name",
+      cost: "2U",
+      type: {
+        supertype: "Creature",
+        subtype: "",
+      },
+      color: "Blue",
+      indexNumber: "2",
+    },
+  ],
+  Black: [
+    {
+      name: "Sample Black Card Name",
+      cost: "2B",
+      type: {
+        supertype: "Creature",
+        subtype: "",
+      },
+      color: "Black",
+      indexNumber: "3",
+    },
+  ],
+  Red: [
+    {
+      name: "Sample Red Card Name",
+      cost: "2R",
+      type: {
+        supertype: "Creature",
+        subtype: "",
+      },
+      color: "Red",
+      indexNumber: "4",
+    },
+  ],
+  Green: [
+    {
+      name: "Sample Green Card Name",
+      cost: "2G",
+      type: {
+        supertype: "Creature",
+        subtype: "",
+      },
+      color: "Green",
+      indexNumber: "5",
+    },
+  ],
+  Gold: [
+    {
+      name: "Sample Gold Card Name",
+      cost: "WB",
+      type: {
+        supertype: "Creature",
+        subtype: "",
+      },
+      color: "Gold",
+      indexNumber: "6",
+    },
+  ],
+  Artifact: [
+    {
+      name: "Sample Artifact Card Name",
+      cost: "2",
+      type: {
+        supertype: "Artifact",
+        subtype: "",
+      },
+      color: "Artifact",
+      indexNumber: "7",
+    },
+  ],
+  Land: [
+    {
+      name: "Sample Land Card Name",
+      cost: "",
+      type: {
+        supertype: "Land",
+        subtype: "",
+      },
+      color: "Land",
+      indexNumber: "8",
+    },
+  ],
+};
